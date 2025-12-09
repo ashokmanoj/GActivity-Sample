662c72abda4d1fe9a5f7ef949e878acc60453e7c
Manoj <manojc6363530865@gmail.com>
2025-12-09 15:19:05 +0530

    Update some info

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,17 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        dark: {
-          bg: "#0f172a",        // main background
-          card: "#1e293b",      // surface/cards
-          border: "#334155",    // card/table border
-          text: "#e2e8f0",      // heading text
-          textMuted: "#94a3b8", // secondary text
-          accent: "#3b82f6",    // primary blue
-          glow: "#60a5fa",      // glow hover
-        }
-      }
+      // colors: {
+      //   dark: {
+      //     bg: "#0f172a",        // main background
+      //     card: "#1e293b",      // surface/cards
+      //     border: "#334155",    // card/table border
+      //     text: "#e2e8f0",      // heading text
+      //     textMuted: "#94a3b8", // secondary text
+      //     accent: "#3b82f6",    // primary blue
+      //     glow: "#60a5fa",      // glow hover
+      //   }
+      // }
     }
   },
   plugins: [],
