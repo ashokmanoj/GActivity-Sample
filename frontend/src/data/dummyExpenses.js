719b4f93895a8325633397e15f2c7b21777348d9
Manoj <manojc6363530865@gmail.com>
2025-12-11 11:23:43 +0530

    Add 2 new dummy data

---
+++ frontend/src/data/dummyExpenses.js
@@ -0,0 +1,59 @@
+export const dummyExpenses = [
+  {
+  id: 1,
+  billdate: "2024-09-8",
+  executive_name: "Rohit Sharma",
+  mobile: "9876543210",
+  no_of_bill: 3,
+  rm_app_count: 2,
+  req_amount: 1450,
+  rm_amount: 1200,
+  rmh_amount: 1100,
+  account: "Cash",
+  status: "Pending",
+  images: [
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80"
+  ]
+},
+{
+  id: 2,
+  billdate: "2024-07-7",
+  executive_name: "Rohit Sharma",
+  mobile: "9876543210",
+  no_of_bill: 3,
+  rm_app_count: 2,
+  req_amount: 1450,
+  rm_amount: 1200,
+  rmh_amount: 1100,
+  account: "Cash",
+  status: "Pending",
+  images: [
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80"
+  ]
+},
+{
+  id: 3,
+  billdate: "2024-08-12",
+  executive_name: "Rohit Sharma",
+  mobile: "9876543210",
+  no_of_bill: 3,
+  rm_app_count: 2,
+  req_amount: 1450,
+  rm_amount: 1200,
+  rmh_amount: 1100,
+  account: "paytm",
+  status: "Pending",
+  images: [
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80",
+    "https://via.placeholder.com/80"
+  ]
+}
+];
