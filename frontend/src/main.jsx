cce260dddf4548807487dd68071227d720970f41
Manoj <manojc6363530865@gmail.com>
2025-12-11 11:31:29 +0530

    Add Map package and some features

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,12 +8,15 @@ import { PageTitleProvider } from "./context/PageTitleContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { LoadScript } from "@react-google-maps/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ThemeProvider>
       <PageTitleProvider>
+      <LoadScript googleMapsApiKey="AIzaSyD-de6OTIs2NRXKOaO6p2M19h1omc0iLzo" >
         <App />
+        
         <ToastContainer
           position="top-center"
           autoClose={1000}
@@ -24,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           draggable
           theme="colored"
         />
+        </LoadScript>
       </PageTitleProvider>
     </ThemeProvider>
   </BrowserRouter>
